36ac451645d3aaa81b1c31b2ec70e4a80c7d54fd
Yu guoyuan <91068173+corookie@users.noreply.github.com>
2026-06-10 10:07:32 +0800

    Fix Pages base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  base: "/Cyber-Divination/",
+  base: "/Cyber-Oracle/",
   plugins: [vue()],
 });
